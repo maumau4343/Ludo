30ef3b893cb6b210d122e0d02c187980e2268bf1
maumau4343 <mauricioleitesilvamaia44@gmail.com>
2024-03-19 20:04:39 -0300

    nooooo

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ useEffect(()=>{
      <br></br><h1>Como {name}:</h1>
      <h2 >Objetivo:</h2>
      <h5>É só matar quem estiver no caminho e chegar no final,<br></br>não esqueça de deixar um guarda matador, simples</h5>
-     <h2>Quantidade de peças: 16</h2><h3><br></br></h3>
+     <h2>Quantidade de peças: 48</h2><h3><br></br></h3>
      
      <h2>Frases<br></br> motivacionais<br></br> para recitar durante o jogo:</h2>
      <Frase03/>
